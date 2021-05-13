ea0ac045090865e2997cfcffd0b24b5a22b14356
MartinWhat <467045478@qq.com>
2021-05-13 23:53:13 +0800

    头像上传

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,6 +19,7 @@ class User extends Authenticatable implements MustVerifyEmailContract
         'email',
         'password',
         'introduction',
+        'avatar',
     ];
 
     protected $hidden = [
