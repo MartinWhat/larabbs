82b0a64ec68039a58b0ee3d9d66c853dca98591f
MartinWhat <467045478@qq.com>
2021-06-16 19:29:53 +0800

    创建话题

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -18,8 +18,8 @@ class UsersTableSeeder extends Seeder
 
         // 单独处理第一个用户的数据
         $user = User::find(1);
-        $user->name = 'Summer';
-        $user->email = 'summer@example.com';
+        $user->name = 'What';
+        $user->email = '467045478@qq.com';
         $user->avatar = 'https://iocaffcdn.phphub.org/uploads/images/201710/14/1/ZqM7iaP4CR.png';
         $user->save();
     }
